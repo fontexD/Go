79b23889c5e7554724547d429206fea01c260b3c
fontexD <87015443+fontexD@users.noreply.github.com>
2022-10-07 06:03:30 +0200

    Update gui2.go

--- myown/gui2.go
+++ myown/gui2.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -12,62 +10,44 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-type MovieResults struct {
-	Results []Movie `json:"results"`
-}
-
-type Movie struct {
-	Name string `json:"Name"`
-}
-
-func LoadMovies() (MovieResults, error) {
-	data, err := ioutil.ReadFile("./buttons.json")
-	if err != nil {
-		return MovieResults{}, err
-	}
-	var movieResults MovieResults
-	err = json.Unmarshal(data, &movieResults)
-	if err != nil {
-		return MovieResults{}, err
-	}
-	return movieResults, nil
-}
-
 func main() {
-
-	moviesResults, err := LoadMovies()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("movies: %s\n", moviesResults)
-
 	a := app.New()
 	a.Settings().SetTheme(theme.DarkTheme())
 	W := a.NewWindow("Application-OutSight")
 	W.Resize(fyne.NewSize(640, 460))
 
-	menuBar := []strings{"Home","App-Status"}
-	
-	listView := widget.NewList(func() int {
-		return len(menuBar)
-	}, func() fyne.CanvasObject {
-		return widget.NewLabel("template")
-	}, func(id widget.ListItemID, o fyne.CanvasObject) {
-		o.(*widget.Label).SetText = (menuBar[id])
-
-	})
+	var menu = []string{"Home", "App-Status", "Exit"}
 
 	contenttext := widget.NewLabel("Welcome to this App")
-	listView.OnSelected = func(id widget.ListItemID) {
-		contenttext.Text = moviesResults.Results[id].Name
-	}
 
 	split := (container.NewHSplit(
-		listView,
+		menuBar(menu),
 		container.NewMax(contenttext),
 	))
 	split.Offset = 0.2
 	W.SetContent(split)
 	W.ShowAndRun()
 
+}
+
+func menuBar(data []string) *widget.List {
+	return widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return container.NewPadded(
+				widget.NewLabel("Will be replaced"),
+				widget.NewButton("", nil),
+			)
+		},
+		func(id widget.ListItemID, o fyne.CanvasObject) {
+			o.(*fyne.Container).Objects[0].(*widget.Label).SetText(data[id])
+
+			// new part
+			o.(*fyne.Container).Objects[1].(*widget.Button).OnTapped.(data[id]) = func() {
+				fmt.Println("I am button " + data[id])
+			}
+		},
+	)
 }
