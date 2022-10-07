457361f06ee5a3da4debabf4e461af4868f3cf11
fontexD <87015443+fontexD@users.noreply.github.com>
2022-10-07 05:51:05 +0200

    Create gui2.go

---
+++ myown/gui2.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+type MovieResults struct {
+	Results []Movie `json:"results"`
+}
+
+type Movie struct {
+	Name string `json:"Name"`
+}
+
+func LoadMovies() (MovieResults, error) {
+	data, err := ioutil.ReadFile("./buttons.json")
+	if err != nil {
+		return MovieResults{}, err
+	}
+	var movieResults MovieResults
+	err = json.Unmarshal(data, &movieResults)
+	if err != nil {
+		return MovieResults{}, err
+	}
+	return movieResults, nil
+}
+
+func main() {
+
+	moviesResults, err := LoadMovies()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("movies: %s\n", moviesResults)
+
+	a := app.New()
+	a.Settings().SetTheme(theme.DarkTheme())
+	W := a.NewWindow("Application-OutSight")
+	W.Resize(fyne.NewSize(640, 460))
+
+	menuBar := []strings{"Home","App-Status"}
+	
+	listView := widget.NewList(func() int {
+		return len(menuBar)
+	}, func() fyne.CanvasObject {
+		return widget.NewLabel("template")
+	}, func(id widget.ListItemID, o fyne.CanvasObject) {
+		o.(*widget.Label).SetText = (menuBar[id])
+
+	})
+
+	contenttext := widget.NewLabel("Welcome to this App")
+	listView.OnSelected = func(id widget.ListItemID) {
+		contenttext.Text = moviesResults.Results[id].Name
+	}
+
+	split := (container.NewHSplit(
+		listView,
+		container.NewMax(contenttext),
+	))
+	split.Offset = 0.2
+	W.SetContent(split)
+	W.ShowAndRun()
+
+}
