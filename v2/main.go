0b5c70f30a6dad4b1aed6acc1d6dbaaaa51d8a52
fontexD <87015443+fontexD@users.noreply.github.com>
2022-10-10 08:32:52 +0200

    Update main.go

--- v2/main.go
+++ v2/main.go
@@ -3,37 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 var Menu = []string{"Home", "App-Status"}
 
+// Define strucutre of data
 type Data struct {
 	Name  string `json:"key"`
 	Value string `json:"value"`
-}
-
-func getData() (Data, error) {
-	var CheckInterval int = 5
-	var Url string = "http://127.0.0.1:8080/health"
-	var fact Data
-	resp, err := http.Get(url)
-	if err != nil {
-		return Data{}, err
-	}
-	defer resp.Body.Close()
-
-	err = json.NewDecoder(resp.Body).Decode(&fact)
-	if err != nil {
-		return Data{}, err
-	}
-	return fact, nil
 }
 
 func main() {
@@ -41,8 +29,8 @@ func main() {
 	a := app.New()
 	a.Settings().SetTheme(theme.DarkTheme())
 	W = a.NewWindow("Application-OutSight")
-	W.Resize(fyne.NewSize(640, 460))
-	text := widget.NewLabel("Welcome to This App")
+	W.Resize(fyne.NewSize(640, 480))
+	text := widget.NewLabel("W½elcome to This App")
 
 	// start container with welcome text
 	listView := widget.NewList(func() int {
@@ -57,15 +45,26 @@ func main() {
 
 	listView.OnSelected = func(id widget.ListItemID) {
 		if id == 0 {
-			text = widget.NewLabel("test2")
-			contain2 := container.NewMax(text)
+			text := widget.NewLabel("Storage.plan2learn.dk")
+			text2 := widget.NewLabel(dataReturn().Value)
+			text3 := canvas.NewImageFromFile("./healthy.png")
+			text3.FillMode = canvas.ImageFillContain
+			text3.SetMinSize(fyne.Size{Width: 30, Height: 20})
+			grid := container.New(layout.NewGridWrapLayout(fyne.Size{Width: 200, Height: 50}), text, text2, text3)
 
 			split := (container.NewHSplit(
 				listView,
-				contain2,
+				grid,
 			))
 			split.Offset = 0.2
 			W.SetContent(split)
+			go func() {
+				for range time.Tick(time.Second * 5) {
+					text2.SetText(dataReturn().Value)
+
+				}
+			}()
+			W.Show()
 		} else if id == 1 {
 			fmt.Println("app")
 		}
@@ -82,4 +81,42 @@ func main() {
 	split.Offset = 0.2
 	W.SetContent(split)
 	W.ShowAndRun()
+}
+
+func dataReturn() Data {
+	//Define var Jsonoutput to what Datastructure it holds
+	var JsonOutput Data
+	var err error
+
+	// Create array of urls to send t function GetData
+	Urls := []string{"http://192.168.10.11:8080/health"}
+
+	// Infinity loop which render every 5 second
+	//loop:
+	for x := range Urls {
+		JsonOutput, err = Getdata(Urls[x])
+		//error checking
+		if err != nil {
+			log.Fatal(err)
+		}
+		// print .Value from Jsonoutput Struct variable
+	}
+	return JsonOutput
+}
+
+// Func to pull data from  http.get, it takes Urls as input/call , Returns
+func Getdata(Urls string) (Data, error) {
+
+	var JsonOutput Data
+	rsp, err := http.Get(Urls)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rsp.Body.Close()
+
+	err = json.NewDecoder(rsp.Body).Decode(&JsonOutput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return JsonOutput, err
 }
